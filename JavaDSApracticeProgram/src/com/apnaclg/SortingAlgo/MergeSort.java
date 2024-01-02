829701dee3ce56a95ae2f6bf18891a7d6c70dce9
akash6rout <akash06.rout@gmail.com>
2024-01-02 23:03:22 +0530

    ms

---
+++ JavaDSApracticeProgram/src/com/apnaclg/SortingAlgo/MergeSort.java
@@ -0,0 +1,13 @@
+package com.apnaclg.SortingAlgo;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {5,2,1,6,3,4};
+		
+		for(int a:arr) {
+			System.out.println(a);
+		}
+	}
+}
