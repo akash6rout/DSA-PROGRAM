d29447f7fb954a3d993a5fd9f99081dbe23a2e95
akash6rout <akash06.rout@gmail.com>
2024-01-03 21:31:17 +0530

    msort

--- JavaDSApracticeProgram/src/com/apnaclg/SortingAlgo/MergeSort.java
+++ JavaDSApracticeProgram/src/com/apnaclg/SortingAlgo/MergeSort.java
@@ -1,13 +1,62 @@
 package com.apnaclg.SortingAlgo;
 
 public class MergeSort {
+	public static void printArr(int arr[]) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void mergeSort(int arr[],int si,int ei){
+        if(si<=ei){
+            return;
+        }
+
+        //work
+        int mid=si+(ei-si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+	public static void merge(int arr[],int si,int mid,int ei) {
+		int temp[]=new int[ei-si+1];
+		int i=si;//this is my iterator for left part
+		int j=mid+1;//iterator for right part
+		int k=0;//iterator for temp array
+		
+		while(i<=mid && j<=ei) {
+			if(arr[i]<=arr[j]) {
+				temp[k]=arr[i];
+				i++;
+			}else {
+				temp[k]=arr[j];
+				j++;
+			}
+			k++;
+		}
+		//left part
+		while(i<=mid) {
+			temp[k++]=arr[i++];
+		}
+		//right part
+		while(j<=ei) {
+			temp[k++]=temp[j++];
+		}
+		
+		//copy temp to original array
+		for(k=0,i=si;k<temp.length;k++,i++) {
+			arr[i]=arr[k];
+		}
+		
+	}
+	
 
 	public static void main(String[] args) {
 		
-		int arr[]= {5,2,1,6,3,4};
+		 int arr[]={6,3,9,5,2,8};
+		 mergeSort(arr, 0, arr.length-1);
+	     printArr(arr);
 		
-		for(int a:arr) {
-			System.out.println(a);
-		}
+		
 	}
 }
