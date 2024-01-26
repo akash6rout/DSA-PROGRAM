c03edcda0008a82729dcd5c0ecf7e89b2a0b1c30
akash6rout <akash06.rout@gmail.com>
2024-01-26 22:38:08 +0530

    p11

--- JavaDSApracticeProgram/src/com/starPattern/sp11.java
+++ JavaDSApracticeProgram/src/com/starPattern/sp11.java
@@ -3,7 +3,16 @@ package com.starPattern;
 public class sp11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=4;i++) {
+			for(int j=4;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<(i*2);k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
