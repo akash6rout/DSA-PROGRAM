b1c1757cb369e776ec7d0b7bd96e570042055c76
akash6rout <akash06.rout@gmail.com>
2024-01-29 23:13:23 +0530

    sp12

--- JavaDSApracticeProgram/src/com/starPattern/StarPattern12.java
+++ JavaDSApracticeProgram/src/com/starPattern/StarPattern12.java
@@ -1,27 +1,22 @@
 package com.starPattern;
 
-public class StarPattern9 {
+public class StarPattern12 {
 
 	public static void main(String[] args) {
-		/*
+		// TODO Auto-generated method stub
+		
 		for(int i=1;i<=4;i++) {
 			for(int j=4;j>=i;j--) {
 				System.out.print(" ");
 			}
-			for(int k=1;k<(i*2);k++) {
+			for(int k=1;k<=i;k++) {
 				System.out.print("*");
 			}
-			System.out.println();
-		}
-		*/
-		for(int i=1;i<=4;i++) {
-			for(int j=4;j>=i;j--) {
-				System.out.print(" ");
-			}
-			for(int k=1;k<(i*2);k++) {
+			for(int l=2;l<=i;l++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
+
 }
